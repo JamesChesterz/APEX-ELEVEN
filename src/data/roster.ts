820e864df88543bb0af7561643f13b7539bd1e05
JamesChesterz <89320582+JamesChesterz@users.noreply.github.com>
2026-08-28 11:45:42 +0700

    Update roster.ts

--- src/data/roster.ts
+++ src/data/roster.ts
@@ -158,7 +158,7 @@ export const ROSTER: RosterEntry[] = [
   { file: 'p123.webp', rarity: 'mythical', name: 'ROONEY', position: 'ST', ovr: 123   },
   { file: 'p124.webp', rarity: 'mythical', name: 'SOCRETES', position: 'CM', ovr: 123   },
   { file: 'p125.webp', rarity: 'mythical', name: 'RONALDO', position: 'ST', ovr: 124 },
-  { file: 'p126.webp', rarity: 'legendary', name: 'CASILLAS', position: 'GK', ovr: 122 },
+  { file: 'p126.webp', rarity: 'legendary', name: 'CASILLAS', position: 'GK', ovr: 120 },
   { file: 'p127.gif', rarity: 'mythical', name: 'BELLINGHAM', position: 'CM', ovr: 123 },
   { file: 'p128.gif', rarity: 'mythical', name: 'LAMINE YAMAL', position: 'RW', ovr: 123 },
   { file: 'p129.gif', rarity: 'mythical', name: 'MBAPPE', position: 'ST', ovr: 123 },
